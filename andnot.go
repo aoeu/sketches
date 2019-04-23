30e9b242406853bd896ae9b10a41d680323761cc
Travis McDemus <678571+aoeu@users.noreply.github.com>
2019-04-23 09:07:15 -0400

    Add example for "and-not" operator

--- andnot.go
+++ andnot.go
@@ -13,4 +13,17 @@ func main() {
 	var three, one uint8 = 3, 1
 	two := three &^ one
 	fmt.Println(two)
+
+	// The operator could arguably be called "left xor" instead of "and-not"
+	// in order to avoid any conflation with "nand" (not-and).
+
+	// This can be a little unexpected, especially because "|^" is not an
+	// operator - the one would be bitwise negated (bit-flipped), then
+	// "or" gated with the number three, resulting in 255 (all 8 bits being high-bits).
+	twoHundredFiftyFive := three | ^one
+	fmt.Println(twoHundredFiftyFive)
+
+	// Note how the formatter forcibly justifies the negate to the right operand.
+	// That help go to how having a universal formatter in a language can help
+	// even in areas where the language syntax can be tricky.
 }
